14038914bb7dbc8430e1af540fdeac7886214287
Kubilay ADA <kubilayada.26@gmail.com>
2025-11-03 16:54:23 +0100

    set API base to backend

--- src/form/DoctorDashboard.tsx
+++ src/form/DoctorDashboard.tsx
@@ -2,7 +2,7 @@
 
 import React, { useState } from 'react'
 
-const API_BASE = process.env.NEXT_PUBLIC_BACKEND_URL || 'http://localhost:3001'
+const API_BASE = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:3001'
 
 interface TreatmentRequest {
   id: number
