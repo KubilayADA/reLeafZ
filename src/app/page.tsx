de53b7122b8c7da1d29be54e0d1d8f1650eb10aa
Kubilay ADA <kubilayada.26@gmail.com>
2026-02-20 15:08:19 +0100

    lock

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,7 +11,7 @@ import Header from './header'
 import '@/components/ui/Hero/Words-Sliding-Smooth.css' 
 import ComingSoon from '@/components/ComingSoon'  
 
-const COMING_SOON_MODE = false;
+const COMING_SOON_MODE = true;
 
 // Font setup - using Inconsolata
 const inconsolataStyle = {
