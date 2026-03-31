f28da3f8e01e3d719bff2504ed2655d9d18dd027
Kubilay ADA <kubilayada.26@gmail.com>
2026-03-31 21:21:08 +0200

    fix(csp): add Google Maps to script-src and frame-src

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,5 @@
-
-
 import type { NextConfig } from "next";
-
 const nextConfig: NextConfig = {
-  /* config options here */
   async headers() {
     return [
       {
@@ -18,22 +14,21 @@ const nextConfig: NextConfig = {
             key: 'Content-Security-Policy',
             value: [
               "default-src 'self'",
-              "script-src 'self' 'unsafe-inline' https://js.stripe.com https://eu-assets.i.posthog.com",
-              "style-src 'self' 'unsafe-inline' https://fonts.googleapis.com",
+              "script-src 'self' 'unsafe-inline' https://js.stripe.com https://eu-assets.i.posthog.com https://maps.googleapis.com",
+              "style-src 'self' 'unsafe-inline' https://fonts.googleapis.com https://maps.googleapis.com",
               "font-src 'self' https://fonts.gstatic.com",
               "img-src 'self' data: https:",
-              "connect-src 'self' https://api.stripe.com https://maps.googleapis.com https://eu.i.posthog.com https://eu-assets.i.posthog.com " + (process.env.NEXT_PUBLIC_API_URL || ''),
-              "frame-src https://js.stripe.com",
+              "connect-src 'self' https://api.stripe.com https://maps.googleapis.com https://maps.google.com https://eu.i.posthog.com https://eu-assets.i.posthog.com " + (process.env.NEXT_PUBLIC_API_URL || ''),
+              "frame-src https://js.stripe.com https://maps.google.com",
+              "worker-src blob:",
             ].join('; '),
           },
         ],
       },
     ]
   },
-    eslint: {
+  eslint: {
     ignoreDuringBuilds: false,
   },
 };
-
-
 export default nextConfig;
